5b7ef625aa92faa87f43baa50d87a3c5a29ea96b
utkarsh parkhi <uparkhi@ph.iitr.ac.in>
2021-07-25 18:31:06 +0530

    final commit

--- constant/db_config.py
+++ constant/db_config.py
@@ -2,7 +2,7 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27018/")
 review_db = client["review_db"]
-review_col = review_db["reviews"]
+review_col = review_db["rev_c_2"]
 
 processed_db = client["process_db"]
-processed_col = processed_db["process_col"]
+processed_col = processed_db["pro_c_2"]
